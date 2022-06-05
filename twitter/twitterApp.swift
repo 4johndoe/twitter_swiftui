f89c2d1443681728787f41f1c26b1401329098f3
bogdan demchenko <bogdandemchenko@gmail.com>
2022-06-05 16:53:35 +0300

    Initial Commit

---
+++ twitter/twitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitterApp.swift
+//  twitter
+//
+//  Created by Bogdan Demchenko on 05.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct twitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
