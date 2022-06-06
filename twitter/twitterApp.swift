2fed0ea111addcd2bf0221f80cf669e5f5adf394
bogdan demchenko <bogdandemchenko@gmail.com>
2022-06-06 23:44:09 +0300

    new tw button

--- twitter/twitterApp.swift
+++ twitter/twitterApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct twitterApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
