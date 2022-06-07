a52a880dd03a921eb559ce966727b312dce44640
bogdan demchenko <bogdandemchenko@gmail.com>
2022-06-07 21:06:28 +0300

    textArea component

--- twitter/Core/UploadTweets/Views/NewTweetView.swift
+++ twitter/Core/UploadTweets/Views/NewTweetView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct NewTweetView: View {
+    @State private var caption = ""
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
         VStack {
             HStack{
                 Button {
-                    print("cancel")
+                    presentationMode.wrappedValue.dismiss()
                 } label: {
                     Text("Cancel")
                         .foregroundColor(Color(.systemBlue))
@@ -24,19 +27,23 @@ struct NewTweetView: View {
                     print("tweet")
                 } label: {
                     Text("Tweet")
+                        .bold()
                         .padding(.horizontal)
                         .padding(.vertical, 8)
                         .background(Color(.systemBlue))
                         .foregroundColor(.white)
-                     
+                        .clipShape(Capsule())
                 }
             }
+            .padding()
             
-            Spacer()
-            
-            HStack{
+            HStack(alignment: .top){
+                Circle()
+                    .frame(width: 64, height: 64)
                 
+                TextArea("What's happening?", text: $caption)
             }
+            .padding()
         }
     }
 }
