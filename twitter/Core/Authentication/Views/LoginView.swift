af0d84ef105ac7887753cb61c710e2814428fe4b
bogdan demchenko <bogdandemchenko@gmail.com>
2022-06-07 22:40:47 +0300

    prepare reg view

--- twitter/Core/Authentication/Views/LoginView.swift
+++ twitter/Core/Authentication/Views/LoginView.swift
@@ -14,24 +14,8 @@ struct LoginView: View {
     var body: some View {
         // parent container
         VStack{
-            
             // header
-            VStack(alignment: .leading){
-                HStack { Spacer() }
-                
-                Text("Hello.")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                
-                Text("Welcome Back.")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-            }
-            .frame(height: 260)
-            .padding(.leading)
-            .background(Color(.systemBlue))
-            .foregroundColor(.white)
-            .clipShape(RoundedShape(corners: [.bottomRight]))
+            AuthHeaderView(title1: "Hello.", title2: "Welcome Back.")
             
             VStack(spacing: 40) {
                 CustomInputField(imageName: "envelope",
--- twitter/Core/Authentication/Views/RegistrationView.swift
+++ twitter/Core/Authentication/Views/RegistrationView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State private var email = ""
+    @State private var username = ""
+    @State private var fullname = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text("Sign up view ...")
+        VStack {
+            // header
+            AuthHeaderView(title1: "Get started.", title2: "Create your account.")
+            
+            VStack(spacing: 40){
+                CustomInputField(imageName: "envelope",
+                                 placeHolderText: "Email",
+                                 text: $email)
+                
+                CustomInputField(imageName: "person",
+                                 placeHolderText: "Username",
+                                 text: $username)
+                
+                CustomInputField(imageName: "person",
+                                 placeHolderText: "Fullname",
+                                 text: $fullname)
+                
+                CustomInputField(imageName: "lock",
+                                 placeHolderText: "Password",
+                                 text: $password)
+            }
+            .padding(32)
+            
+            Spacer()
+        }.ignoresSafeArea()
     }
 }
 
