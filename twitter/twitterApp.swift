8a2e665d225ea1fe47543198326e8d0df6ec82db
bogdan demchenko <bogdandemchenko@gmail.com>
2022-06-07 22:22:01 +0300

    check login view

--- twitter/twitterApp.swift
+++ twitter/twitterApp.swift
@@ -12,7 +12,8 @@ struct twitterApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                ContentView()
+//                ContentView()
+                LoginView()
             }
         }
     }
