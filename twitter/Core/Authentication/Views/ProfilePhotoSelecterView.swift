15155151113c211a3f97d8bd04148c2a31fd9d33
bogdan demchenko <bogdandemchenko@gmail.com>
2022-06-11 12:31:19 +0300

    added imagePicker

--- twitter/Core/Authentication/Views/ProfilePhotoSelecterView.swift
+++ twitter/Core/Authentication/Views/ProfilePhotoSelecterView.swift
@@ -8,26 +8,53 @@
 import SwiftUI
 
 struct ProfilePhotoSelecterView: View {
+    @State private var showImagePicker = false
+    @State private var selectedImage: UIImage?
+    @State private var profileImage: Image?
+    
     var body: some View {
         VStack{
-            AuthHeaderView(title1: "Create yout account",
+            AuthHeaderView(title1: "Setup account",
                            title2: "Select a profile photo")
             
             Button {
-                print("Pick image here")
+                showImagePicker.toggle()
             } label: {
-                Image(systemName: "photo")
-                    .resizable()
-                    .renderingMode(.template)
-                    .foregroundColor(Color(.systemBlue))
-                    .scaledToFit()
-                    .frame(width: 160, height: 160)
-                    .padding(.top, 44)
+                if let profileImage = profileImage {
+                    profileImage
+                        .resizable()
+                        .scaledToFill()
+                        .clipShape(Circle())
+                        .modifier(ProfileImageModifier())
+                } else {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .scaledToFit()
+                        .modifier(ProfileImageModifier())
+                }
             }
+            .sheet(isPresented: $showImagePicker,
+                   onDismiss: loadImage) {
+                ImagePicker(selectedImage: $selectedImage)
+            }
+            .padding(.top, 44)
             
             Spacer()
         }
         .ignoresSafeArea()
+    }
+    
+    func loadImage() {
+        guard let selectedImage = selectedImage else { return }
+        profileImage = Image(uiImage: selectedImage)
+    }
+}
+
+private struct ProfileImageModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(Color(.systemBlue))
+            .frame(width: 160, height: 160)
     }
 }
 
